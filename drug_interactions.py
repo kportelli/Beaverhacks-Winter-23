a2505f8ae69cbd5bef46dc5b94f0a56e1bf8508c
Kathryn Portelli <kathrynp163@gmail.com>
2023-01-13 18:26:04 -0900

    Set up list to contain RxCUI numbers, defined function to find RxCUi number given drug name

--- drug_interactions.py
+++ drug_interactions.py
@@ -1,7 +1,24 @@
 import requests
 import json
 
-drug_name = input('input drug name')
-api_url = (f'https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui={drug_name}&sources=DrugBank')
-response = requests.get(api_url)
-print(json.dumps(response.json(), indent=2))
+patient_drugs = []
+def main():
+    drug_name = input('input drug name')
+    rxcui = drug_id_finder(drug_name)
+    if rxcui != False:
+        patient_drugs.append(rxcui)
+    else:
+        print("invalid drug name")
+    print(patient_drugs)
+def drug_id_finder(drug_name):
+    api_url = (f'https://rxnav.nlm.nih.gov/REST/rxcui.json?name={drug_name}')
+    response = requests.get(api_url)
+    try:
+        response = response.json()
+        rxcui = response["idGroup"]["rxnormId"][0]
+    except:
+        return False
+    return rxcui
+
+if __name__ == '__main__':
+  main()
