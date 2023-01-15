174fe027a7b12305439bca76d6aba7774c8df562
Kathryn Portelli <kathrynp163@gmail.com>
2023-01-14 15:47:00 -0900

    Added a couple comments

--- drug_interactions.py
+++ drug_interactions.py
@@ -3,13 +3,17 @@ import json
 
 
 def main():
-    """?"""
+    #disclaimers
     print('It is not this applications intention to provide specific medical advice, but rather to provide users with information to better understand their health and their medications. You should consult with a qualified physician for advice about medications.', end='\n\n')
     print("This application is intended for educational and scientific research purposes only and you expressly acknowledge and agree that use of this application is at your sole risk. The accuracy of this application's information is not guaranteed and reliance on this application shall be at your sole risk. This application is not intended as a substitute for professional medical advice, diagnosis or treatment.", end='\n\n')
     
+    #store given user medications rxcui numbers
     patient_drugs = ["10171", '153010']
 
+
+    #prompt user for medication names (this will be a loop)
     drug_name = input('Input drug name: ')
+    #pull rxcui data, else reprompt
     rxcui = drug_id_finder(drug_name)
     if rxcui != False:
         patient_drugs.append(rxcui)
@@ -20,7 +24,7 @@ def main():
 
 
 def drug_id_finder(drug_name):
-    """returns the rxcui of a given drug"""
+    """returns the rxcui of a given medication"""
     api_url = (f'https://rxnav.nlm.nih.gov/REST/rxcui.json?name={drug_name}')
     response = requests.get(api_url)
     try:
@@ -31,7 +35,7 @@ def drug_id_finder(drug_name):
     return rxcui
 
 
-#Once RxCUI numbers are in the list, start iterating through the list and check for references of all other drugs in the list
+#Iterating through list of patient meds, pull interaction data
 def contra_checker(patient_drugs):
     """Check all drugs in users drug list against each other for any interactions"""
     for i, drug in enumerate(patient_drugs[:len(patient_drugs) - 1]):
