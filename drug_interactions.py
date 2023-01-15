3435206131a5df9500de92fc6036d8452fa69a5a
Tyson Pederson <tysonpederson95@gmail.com>
2023-01-14 21:20:37 -0600

    Add disclaimers on start of application

--- drug_interactions.py
+++ drug_interactions.py
@@ -4,9 +4,12 @@ import json
 
 def main():
     """?"""
+    print('It is not this applications intention to provide specific medical advice, but rather to provide users with information to better understand their health and their medications. You should consult with a qualified physician for advice about medications.', end='\n\n')
+    print("This application is intended for educational and scientific research purposes only and you expressly acknowledge and agree that use of this application is at your sole risk. The accuracy of this application's information is not guaranteed and reliance on this application shall be at your sole risk. This application is not intended as a substitute for professional medical advice, diagnosis or treatment.", end='\n\n')
+    
     patient_drugs = ["10171", '153010']
 
-    drug_name = input('input drug name: ')
+    drug_name = input('Input drug name: ')
     rxcui = drug_id_finder(drug_name)
     if rxcui != False:
         patient_drugs.append(rxcui)
@@ -37,8 +40,10 @@ def contra_checker(patient_drugs):
         interaction_pairs = response["interactionTypeGroup"][0]["interactionType"][0]["interactionPair"]
         for pair in interaction_pairs:
             pair_rxcui = pair["interactionConcept"][1]["minConceptItem"]["rxcui"]
+            pair_name = pair["interactionConcept"][1]["minConceptItem"]["name"]
+            drug_name = pair["interactionConcept"][0]["minConceptItem"]["name"]
             if pair_rxcui in patient_drugs[i+1:]:
-                print(pair["description"])
+                print(f'{drug_name.capitalize()} may interact with {pair_name.capitalize()}, you may want to talk your doctor.')
 
 
 if __name__ == '__main__':
