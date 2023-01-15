80f2c18e6f2a3772ea329768f17839a0e5f2bd96
Kathryn Portelli <kathrynp163@gmail.com>
2023-01-14 17:42:58 -0900

    Prevented duplicated medications within list.

--- drug_interactions.py
+++ drug_interactions.py
@@ -1,9 +1,10 @@
+#Beaverhacks Winter 2023 Hackathon
+#Theme "Health"
+#Team WYCM
+#Tyson Pederson, Kathryn Portelli, Brittany Stachkunis, Hannah Weeks
+
 import requests
-import json
-
-
 def main():
-    """?"""
     print('It is not this applications intention to provide specific medical advice, but rather to provide users with information to better understand their health and their medications. You should consult with a qualified physician for advice about medications.', end='\n\n')
     print("This application is intended for educational and scientific research purposes only and you expressly acknowledge and agree that use of this application is at your sole risk. The accuracy of this application's information is not guaranteed and reliance on this application shall be at your sole risk. This application is not intended as a substitute for professional medical advice, diagnosis or treatment.", end='\n\n')
 
@@ -11,20 +12,22 @@ def main():
     count = contra_checker(patient_drugs)
     print('There were', count,'interactions found.')
 
-
 def generate_list():
-    patient_drugs = ["10171", '153010']
+    #"10171" advil, '153010'
+    patient_drugs = []
     response = "y"
     while response == "y":
         drug_name = input('Input drug name: ')
         rxcui = drug_id_finder(drug_name)
-        if rxcui != False:
+        if rxcui != False and rxcui not in patient_drugs:
             patient_drugs.append(rxcui)
             response = input('Add another medication? Enter Y or N: ').lower()
             while response != "n" and response != "y":
                 response = input('Response must be Y or N: ').lower()
+        elif rxcui in patient_drugs:
+            print("Please do not duplicate drug names.")
         else:
-            print("invalid drug name")
+            print("Invalid drug name.")
     return patient_drugs
 
 
@@ -40,7 +43,6 @@ def drug_id_finder(drug_name):
     return rxcui
 
 
-#Once RxCUI numbers are in the list, start iterating through the list and check for references of all other drugs in the list
 def contra_checker(patient_drugs):
     """Check all drugs in users drug list against each other for any interactions"""
     count = 0
