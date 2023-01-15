0d5dffe736df3b6d95756b20ad1d73f9a0965fe4
Tyson Pederson <tysonpederson95@gmail.com>
2023-01-15 05:58:10 -0600

    Change filename to cura.py, utilize findInteractionsFromList api to find interactions, let user write done if they don't want to input another drug name.

--- cura.py
+++ cura.py
@@ -4,11 +4,15 @@
 #Tyson Pederson, Kathryn Portelli, Brittany Stachkunis, Hannah Weeks
 
 import requests
+import sys
 
 def main():
     print('It is not this applications intention to provide specific medical advice, but rather to provide users with information to better understand their health and their medications. You should consult with a qualified physician for advice about medications.', end='\n\n')
     print("This application is intended for educational and scientific research purposes only and you expressly acknowledge and agree that use of this application is at your sole risk. The accuracy of this application's information is not guaranteed and reliance on this application shall be at your sole risk. This application is not intended as a substitute for professional medical advice, diagnosis or treatment.", end='\n\n')
     patient_drugs = generate_list()
+    if len(patient_drugs) == 0:
+        print('No drugs were input')
+        sys.exit()
     count = contra_checker(patient_drugs)
     print('There were', count,'interactions found.')
 
@@ -18,7 +22,7 @@ def generate_list():
     response = "y"
     #prompt user for drug name, reprompt if invalid name given
     while response == "y":
-        drug_name = input('Input drug name: ')
+        drug_name = input('Input drug name, or "done" if done: ')
         rxcui = drug_id_finder(drug_name)
         if rxcui != False and rxcui not in patient_drugs:
             patient_drugs.append(rxcui)
@@ -27,6 +31,8 @@ def generate_list():
                 response = input('Response must be Y or N: ').lower()
         elif rxcui in patient_drugs:
             print("Please do not duplicate drug names.")
+        elif drug_name.lower() == 'done':
+            break
         else:
             print("Invalid drug name.")
     return patient_drugs
@@ -45,20 +51,21 @@ def drug_id_finder(drug_name):
 def contra_checker(patient_drugs):
     """Check all drugs in users drug list against each other for any interactions"""
     count = 0
-    for i, drug in enumerate(patient_drugs[:len(patient_drugs) - 1]):
-        #create a list of interaction pairs for a specific drug in list
-        url = (f'https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui={drug}')
-        response = requests.get(url).json()
-        interaction_pairs = response["interactionTypeGroup"][0]["interactionType"][0]["interactionPair"]
-        #cross check every interaction pair against the remaining drugs in the list
-        for pair in interaction_pairs:
-            pair_rxcui = pair["interactionConcept"][1]["minConceptItem"]["rxcui"]
-            pair_name = pair["interactionConcept"][1]["minConceptItem"]["name"]
-            drug_name = pair["interactionConcept"][0]["minConceptItem"]["name"]
-            #if an interaction pair is found, print warning message
-            if pair_rxcui in patient_drugs[i+1:]:
-                print(f'{drug_name.capitalize()} may interact with {pair_name.capitalize()}, you may want to talk your doctor.')
-                count = count + 1
+    # Format list of drugs to be usable with api
+    patient_drugs = '+'.join(patient_drugs)
+    url = f'https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis={patient_drugs}'
+    response = requests.get(url).json()
+    # Try to find interactions, if none are available return 0
+    try:
+        interaction_pairs = response['fullInteractionTypeGroup'][0]['fullInteractionType']
+    except KeyError:
+        return count
+    # Print a warning with both drug names for each interaction found
+    for pair in interaction_pairs:
+        drug1 = pair['interactionPair'][0]['interactionConcept'][0]['minConceptItem']['name'].capitalize()
+        drug2 = pair['interactionPair'][0]['interactionConcept'][1]['minConceptItem']['name'].capitalize()
+        print(f'{drug1} may interact with {drug2}, you may want to talk to your doctor.')
+        count += 1
     return count
 
 if __name__ == '__main__':
